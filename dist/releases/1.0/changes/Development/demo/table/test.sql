e59f72d35218285aa18957bfa258fbc738920496
Promotek <philipp.dahlem@netcologne.com>
2025-03-11 08:56:37 +0100

    Repo leeren

--- dist/releases/1.0/changes/Development/demo/table/test.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
--- changeset DEMO:b6b5b2431d22c362a005cc3da0e674f13306d5d0 stripComments:false logicalFilePath:Development/demo/table/test.sql
--- sqlcl_snapshot src/database/demo/tables/test.sql:null:b6b5b2431d22c362a005cc3da0e674f13306d5d0:create
-
-create table demo.test (
-    test_1 number,
-    test_2 number
-);
--- dist/releases/1.0/changes/Development/demo/table/test_3.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
--- changeset DEMO:fdfbfda71756576a28d1108b9ed22559375528fd stripComments:false logicalFilePath:Development/demo/table/test_3.sql
--- sqlcl_snapshot src/database/demo/tables/test_3.sql:null:fdfbfda71756576a28d1108b9ed22559375528fd:create
-
-create table demo.test_3 (
-    test_1 number,
-    test_2 number
-);
