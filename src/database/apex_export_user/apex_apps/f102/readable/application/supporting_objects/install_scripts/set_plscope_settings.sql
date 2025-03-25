2dd8060768965f70390fcafc8404f88408cd8fe1
Promotek <philipp.dahlem@netcologne.com>
2025-03-25 10:09:27 +0100

    Basis Release

---
+++ src/database/apex_export_user/apex_apps/f102/readable/application/supporting_objects/install_scripts/set_plscope_settings.sql
@@ -0,0 +1,4 @@
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+
+
+-- sqlcl_snapshot {"hash":"a6f8c1f7f5932cbbd0e3535ba25dac715e4fb554","type":"APEX_APPLICATION","name":"f102","schemaName":"APEX_EXPORT_USER"}
