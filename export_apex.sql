a33a6c4cfdf8eaa072370727fc471ed84dd15092
Promotek <philipp.dahlem@netcologne.com>
2025-04-15 16:14:17 +0200

    Staging APEX

--- export_apex.sql
+++ export_apex.sql
@@ -1,13 +1,29 @@
-!git fetch origin;
-project export -o apex.2022;
-!git add .
-!git commit -m "Exporting APEX 2022 project";
-!git push
+set serveroutput on;
+--Input Variablen
+var app_id varchar2(50);
+--exec :app_id :=&1;
+exec :app_id :='&1';
+var version_id varchar2(50);
+exec :version_id :='&1' || '_' || '&2';
+var commit_msg varchar2(50);
+exec :commit_msg :='&3';
+/*
+-- Immer in den develop Branch wechseln und aktuellen Stand ziehen
+!git chekout develop
+!git pull origin*/
+-- Danach den Export des Projektes anstoßen
+--project export -o apex.&app_id.;
+
+prompt &1
+print version_id
+prompt &3
+
 project stage
 !git add .
-!git commit -m "Staging APEX 2022 project";
+!git commit -m "Staging APEX " || version_id;
 !git push
-project release -version 2022_2_0001;
+/*
+project release -version &app_id2_0001;
 !git add .
 !git commit -m "Release APEX 2022 project";
 !git push
@@ -15,3 +31,4 @@ project gen-artifact -version 2022_2_0001;
 !git add .
 !git commit -m "Generating APEX 2022 artifact";
 !git push
+*/
