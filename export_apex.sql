4d6aecb3bdb2d0bae0a8b343df061bf809d58e33
Promotek <philipp.dahlem@netcologne.com>
2025-04-15 16:48:14 +0200

    Tester

--- export_apex.sql
+++ export_apex.sql
@@ -1,11 +1,11 @@
 set serveroutput on;
 
 define version_id1 = "&1._&2";
-define commmit_msg = "Test &1";
+define commmit_msg = "Export APEX APP: &1";
 project export -o apex.&1;
 project stage
 project release -version &version_id1;
 project gen-artifact -version &version_id1;
 !git add .
-!git commit -m "Export APEX APP: &commmit_msg";
+!git commit -m "&3";
 !git push
