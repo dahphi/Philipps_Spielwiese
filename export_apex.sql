29c9738a00ff6c098114d659ce29ea0793d1d549
Promotek <philipp.dahlem@netcologne.com>
2025-04-17 08:40:47 +0200

    Project INIT in NMC Ordner
    NMC Ordner Commit

--- export_apex.sql
+++ export_apex.sql
@@ -1,11 +1,11 @@
 set serveroutput on;
 
 define version_id1 = "&1._&2";
-define commmit_msg = "Export APEX APP: &1";
+define commmit_msg = "&3";
 project export -o apex.&1;
 project stage
 project release -version &version_id1;
 project gen-artifact -version &version_id1;
 !git add .
-!git commit -m "&3";
+!git commit -m &commmit_msg;
 !git push
