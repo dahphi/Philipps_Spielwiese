0e7f60724aa210f65fd3f84c08b05f698d53dede
Philipp Dahlem <philipp.dahlem@netcologne.com>
2026-03-26 22:15:40 +0100

    stage 2

---
+++ SQLCL_TEST/dist/releases/next/changes/SCDP/apex_export_user/comments/locations.sql
@@ -0,0 +1,32 @@
+-- liquibase formatted sql
+-- changeset apex_export_user:1774559729160 stripComments:false logicalFilePath:SCDP/apex_export_user/comments/locations.sql runAlways:false runOnChange:false replaceIfExists:true failOnError:true
+-- sqlcl_snapshot SQLCL_TEST/src/database/apex_export_user/comments/locations.sql:null:be8e1ffa6932f66a654536543eb18cd2339c849e:create
+
+comment on table apex_export_user.locations is
+    'Locations table that contains specific address of a specific office,
+warehouse, and/or production site of a company. Does not store addresses /
+locations of customers. references with the departments and countries tables. ';
+
+comment on column apex_export_user.locations.city is
+    'A not null column that shows city where an office, warehouse, or 
+production site of a company is located. ';
+
+comment on column apex_export_user.locations.country_id is
+    'Country where an office, warehouse, or production site of a company is
+located. Foreign key to country_id column of the countries table.';
+
+comment on column apex_export_user.locations.location_id is
+    'Primary key of locations table';
+
+comment on column apex_export_user.locations.postal_code is
+    'Postal code of the location of an office, warehouse, or production site 
+of a company. ';
+
+comment on column apex_export_user.locations.state_province is
+    'State or Province where an office, warehouse, or production site of a 
+company is located.';
+
+comment on column apex_export_user.locations.street_address is
+    'Street address of an office, warehouse, or production site of a company.
+Contains building number and street name';
+
---
+++ SQLCL_TEST/dist/releases/next/changes/SCDP/apex_export_user/sequences/departments_seq.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+-- changeset APEX_EXPORT_USER:1774559729342 stripComments:false logicalFilePath:SCDP/apex_export_user/sequences/departments_seq.sql runAlways:false runOnChange:false replaceIfExists:true failOnError:true
+-- sqlcl_snapshot SQLCL_TEST/src/database/apex_export_user/sequences/departments_seq.sql:null:aae2870f75a8d2dd04a9bdb70a28bac492c3fcad:create
+
+create sequence apex_export_user.departments_seq minvalue 1 maxvalue 9990 increment by 10 start with 280 nocache noorder nocycle nokeep
+noscale global;
+
