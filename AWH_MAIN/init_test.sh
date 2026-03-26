47ca8440c2088a4451cfdd6375ddc86f55b2b980
Philipp Dahlem <philipp.dahlem@netcologne.com>
2026-03-26 23:27:19 +0100

    init

---
+++ AWH_MAIN/init_test.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+# Connect to Oracle database (non-interactive, just for demonstration)
+sql AWH_MAIN/Uc9EOWz7886mtwzipZpVpQoYvJRbvm@scdt.ora.netcologne.intern:1521/scdt.netcologne.intern <<EOF
+-- You can put SQL commands here if needed
+
+
+Project export
+!git add .
+!git commit -m "export"
+!git push
+
+Project stage
+!git add .
+!git commit -m "stage"
+!git push
+
+
+project release -version 1.1 -verbose
+!git add .
+!git commit -m "release"
+!git push
+
+project gen-artifact -version 1.1
+!git add .
+!git commit -m "artifact"
+!git push
+
+EXIT
+EOF
